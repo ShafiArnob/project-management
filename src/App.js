5031a8e726e7563e10cc3b2ead5d4e33efb0a867
ShafiArnob <shafiarnob@gmail.com>
2022-09-27 18:30:13 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {Routes,Route,Navigate} from 'react-router-dom'
+//pages
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
+import Project from './pages/project/Project'
+// import Navbar from './components/Navbar';
+
+//css
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <Routes>
+          <Route exact path='/' element={<Dashboard/>}></Route>
+          <Route exact path='/create' element={<Create/>}></Route>
+          <Route path='/project/:id'element = {<Project/>}></Route>
+          <Route exact path='/login' element={<Login/>}></Route>
+          <Route exact path='/signup' element={<Signup/>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 import {AuthContextProvider} from './context/AuthContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
