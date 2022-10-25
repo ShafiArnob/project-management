b32f9032774034c7d663fd16d56bd293da00bdb4
ShafiArnob <shafiarnob@gmail.com>
2022-10-26 00:14:49 +0600

    signup ui

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,10 +1,38 @@
 import './Signup.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+  
   return (
-    <div>Signup</div>
+    <form className='auth-form'>
+      <h2>Sign Up</h2>  
+      <label>
+        <span>email:</span>
+        <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} required/>
+      </label>  
+
+      <label>
+        <span>password:</span>
+        <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} required/>
+      </label>  
+      
+      <label>
+        <span>display-name:</span>
+        <input type="text" onChange={(e)=>setDisplayName(e.target.value)} value={displayName} required/>
+      </label>
+      
+      <label>
+        <span>profile thumbnail:</span>
+        <input type="file"  required/>
+      </label>
+
+      <button className='btn'>Sign up</button>
+    </form>
   )
 }
 
