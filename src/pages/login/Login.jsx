801710acb6ff7a34535449ca9fa169c59d4ee7eb
ShafiArnob <shafiarnob@gmail.com>
2022-10-26 20:48:04 +0600

    login ui

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
 import './Login.css'
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+  }
+
   return (
-    <div>Login</div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>  
+      <label>
+        <span>email:</span>
+        <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} required/>
+      </label>  
+
+      <label>
+        <span>password:</span>
+        <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} required/>
+      </label>  
+        
+      <button className='btn'>Login</button>
+
+      {/* {error && <div className='error'>{error}</div>} */}
+    </form>
   )
 }
 
