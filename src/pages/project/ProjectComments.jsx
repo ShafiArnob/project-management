358ac5afa9968359f50b80affbfa76608ca24da3
ShafiArnob <shafiarnob@gmail.com>
2022-11-01 10:56:30 +0600

    showing comments

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -3,6 +3,7 @@ import { useAuthContext } from '../../hooks/useAuthContext'
 import {useFirestore} from '../../hooks/useFirestore'
 import { useState } from 'react'
 
+import Avatar from '../../components/Avatar'
 
 const ProjectComments = ({project}) => {
   const {updateDocument, response} = useFirestore('projects')
@@ -28,6 +29,25 @@ const ProjectComments = ({project}) => {
 
   return (
     <div className='project-comments'>
+      <h4>Project Comments: </h4>
+
+      <ul>
+        {project.comments.length>0 && project.comments.map(comment=>(
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL}/>
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new Comment:</span>
