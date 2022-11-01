0e84ec43ae8377a30084d9e96f8079fb6eaad8d5
ShafiArnob <shafiarnob@gmail.com>
2022-11-01 16:00:26 +0600

    project filter ui component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import {useCollection} from '../../hooks/useCollection'
 import ProjectList from '../../components/ProjectList'
 import './Dashboard.css'
+import ProjectFilter from './ProjectFilter'
 
 function Dashboard() {
   const {documents, error} = useCollection('projects')
@@ -9,6 +10,7 @@ function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {documents && <ProjectFilter/>}
       {documents && <ProjectList projects={documents}/>}
     </div>
   )
